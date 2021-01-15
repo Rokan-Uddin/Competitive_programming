b76f1d39b9380569d64e048ca5a0e39a0f073a3f
Rokan Uddin <rokanu55@gmail.com>
2021-01-15 22:59:10 +0000

    spoj

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 int main() {
-  cout << "Hello !\n";
+  optimize();
+  int n,q;
+  cin>>n>>q;
+  vector<int>v(n);
+  for(int i=0;i<n;i++) cin>>v[i];
+  while(q--) {
+    int val;
+    cin>>val;
+    int lo= lower_bound(v.begin(), v.end(), val)-v.begin();
+    if(!binary_search(v.begin(),v.end(), val)) cout<<"-1"<<endl;
+    else cout<<lo<<endl;
+  }
 }
