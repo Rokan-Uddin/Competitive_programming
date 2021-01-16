870ad76a752e5b654e72cbd0794421cb12534f47
Rokan Uddin <rokanu55@gmail.com>
2021-01-16 13:03:21 +0000

    https://www.spoj.com/problems/RPLC/

--- main.cpp
+++ main.cpp
@@ -1,29 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-long long getSum(long long n){
-  return (n*(n+1)) >> 1;
-}
-int main() 
+#define endl '\n'
+
+const int mx = 1e6+123;
+long long a[mx];
+int n;
+
+bool check ( long long val )
 {
-  optimize();
-    int t;
-    cin>>t;
-    while(t--) {
-      long long y;
-      cin>>y;
-      long long l=1,r=1e5, ans =-1;
-      while(l<=r) {
-        long long mid=(l+r) >> 1;
-        if(getSum(mid)==y) {
-          ans= mid;
-          break;
-        }
-        else if(getSum(mid) <y) { l=mid+1; }
-        else r=mid-1;
-      }
-      if(ans==-1) cout<<"NAI"<<endl;
-      else cout<<ans<<endl;
+    for ( int i = 1; i <= n; i++ ) {
+        val += a[i];
+        if ( val <= 0 ) return 0;
     }
+
+    return 1;
+}
+
+int main()
+{
+    optimize();
+
+    int T;
+    cin  >> T;
+    for ( int tc = 1; tc <= T; tc++ ) {
+        cin >> n;
+        for ( int i = 1; i <= n; i++ ) cin >> a[i];
+        long long l = 1, r = 1e14, ans;
+
+        while ( l <= r ) {
+            long long mid = ( l + r ) >> 1;
+
+            if ( check ( mid ) ) {
+                ans = mid;
+                r = mid-1;
+            }
+            else l = mid+1;
+        }
+
+        cout << "Scenario #" << tc << ": " << ans << endl;
+    }
+    return 0;   
 }
