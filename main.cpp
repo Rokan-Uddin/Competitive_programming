a7ee46f7b9c566bcd97bcec0eced7a88520a1f5d
Rokan Uddin <rokanu55@gmail.com>
2021-01-16 13:12:39 +0000

    spoj Sabbir and Game

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define endl '\n'
+#define endl '\n';
+typedef long long ll;
+const ll infLL = 9000000000000000000;
 
-const int mx = 1e6+123;
-long long a[mx];
-int n;
+const int mx = 1e3+123;
+int a[mx], n;
 
-bool check ( long long val )
+bool check ( ll val )
 {
     for ( int i = 1; i <= n; i++ ) {
         val += a[i];
@@ -21,24 +22,29 @@ int main()
 {
     optimize();
 
-    int T;
-    cin  >> T;
-    for ( int tc = 1; tc <= T; tc++ ) {
+    int t;
+    cin >> t;
+
+    while ( t-- ) {
+
         cin >> n;
-        for ( int i = 1; i <= n; i++ ) cin >> a[i];
-        long long l = 1, r = 1e14, ans;
+        for ( int i = 1; i <= n ; i++ ) cin >> a[i];
 
+        ll l = 0, r = 10e10+123;
+
+        ll ans = infLL;
         while ( l <= r ) {
-            long long mid = ( l + r ) >> 1;
+            ll mid = ( l + r ) >> 1;
 
-            if ( check ( mid ) ) {
-                ans = mid;
+            if ( check( mid ) ) {
+                ans = min ( ans, mid );
                 r = mid-1;
             }
             else l = mid+1;
         }
 
-        cout << "Scenario #" << tc << ": " << ans << endl;
+        cout << ans << endl;
     }
-    return 0;   
+
+    return 0;
 }
