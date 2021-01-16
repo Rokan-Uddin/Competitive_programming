fcc8e60923404520a853ff357c83b657dd58c6c2
Rokan Uddin <rokanu55@gmail.com>
2021-01-16 12:09:33 +0000

    Math is love

--- main.cpp
+++ main.cpp
@@ -2,17 +2,28 @@
 using namespace std;
 #define endl '\n'
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-int main() {
+long long getSum(long long n){
+  return (n*(n+1)) >> 1;
+}
+int main() 
+{
   optimize();
-  int n,q;
-  cin>>n>>q;
-  vector<int>v(n);
-  for(int i=0;i<n;i++) cin>>v[i];
-  while(q--) {
-    int val;
-    cin>>val;
-    int lo= lower_bound(v.begin(), v.end(), val)-v.begin();
-    if(!binary_search(v.begin(),v.end(), val)) cout<<"-1"<<endl;
-    else cout<<lo<<endl;
-  }
+    int t;
+    cin>>t;
+    while(t--) {
+      long long y;
+      cin>>y;
+      long long l=1,r=1e5, ans =-1;
+      while(l<=r) {
+        long long mid=(l+r) >> 1;
+        if(getSum(mid)==y) {
+          ans= mid;
+          break;
+        }
+        else if(getSum(mid) <y) { l=mid+1; }
+        else r=mid-1;
+      }
+      if(ans==-1) cout<<"NAI"<<endl;
+      else cout<<ans<<endl;
+    }
 }
