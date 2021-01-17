a2b82ca40288cd2212cd60b8a11d01a45bce687d
Rokan Uddin <rokanu55@gmail.com>
2021-01-17 23:04:30 +0000

    Expanding Rods
    LOJ-1137
    Udebug

--- main.cpp
+++ main.cpp
@@ -1,31 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
 #define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
 
-double getPQ(double m,double n) {
-  return sqrt((m*m)-(n*n));
-}
 
 int main()
 {
     optimize();
-    int t;
-    cin >> t;
-    for(int j=1;j<=t;j++) {
-      double x,y,c;
-      cin>>x>>y>>c;
-      double l=0,r=x;
-      for(int i=0;i<100;i++) {
-        double mid=(l+r)/2;
-        double p =getPQ(x,mid);
-        double q= getPQ(y,mid);
-        double cc = 1/((1/p)+(1/q));
-        if(cc>c) l=mid;
-        else r=mid;
-      }
-      fraction();
-      cout<<"Case "<<j<<": "<<l<<endl;
- }
+
+    int T;
+    cin >> T;
+    for ( int tc = 1; tc <= T; tc++ ) {
+        double L, n, c;
+        cin >> L >> n >> c;
+
+        double L1 = ( 1 + (n * c) ) * L;
+
+        double l = 0, r = 1e18;
+
+        for ( int i = 0; i < 100; i++ ) {
+            double R = ( l + r ) / 2;
+            double ans = R * 2.0 * asin ( L / ( 2.0 * R ) );
+
+            if ( ans < L1 ) {
+                r = R;
+            }
+            else l = R;
+        }
+
+        double R = l;
+        double h = R - sqrt ( ( R * R ) - ( ( L / 2.0 ) * ( L / 2.0 ) ) );
+
+        fraction();
+        cout << "Case " << tc << ": " << h << endl;
+    }
+
+    return 0;
 }
