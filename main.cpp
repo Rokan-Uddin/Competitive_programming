7e7886170cc686319a4b7fe5a5d22b8f977ef6e5
Rokan Uddin <rokanu55@gmail.com>
2021-01-17 21:31:36 +0000

    https://lightoj.com/problem/triangle-partitioning

--- main.cpp
+++ main.cpp
@@ -1,28 +1,43 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define endl '\n';
-typedef long long ll;
-const ll infLL = 9000000000000000000;
-const int mx = 1e3+123;
-int a[mx], n;
+#define endl '\n'
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
 int main()
 {
     optimize();
-    int n,val,t;
-    cin>>n;
-     vector<int>v(n+1);
-    v[0]=0;
-    for(int i=1;i<=n;i++) {
-      cin>>val;
-      v[i]= val+v[i-1];
+    int T;
+    cin >> T;
+    for ( int tc = 1; tc <= T; tc++ ) {
+        double AB, AC, BC, R;
+
+        cin >> AB >> AC >> BC >> R;
+
+        double l = 0, r = AB;
+
+        for ( int i = 0; i < 100; i++ ) {
+            double AD = ( l + r ) / 2.0;
+            double x = AD / AB;
+
+            double AE = AC * x;
+            double DE = BC * x;
+
+            double pABC = ( AB + BC + AC ) / 2;
+            double pADE = ( AD + AE + DE ) / 2;
+
+            double areaTriABC =  sqrt ( pABC * ( pABC - AB ) * ( pABC - AC ) * ( pABC - BC ) );
+            double areaTriADE =  sqrt ( pADE * ( pADE - AD ) * ( pADE - AE ) * ( pADE - DE ) );
+
+            double areaTra = areaTriABC - areaTriADE;
+
+            if ( R < ( areaTriADE / areaTra ) ) r = AD;
+            else l = AD;
+        }
+
+        fraction();
+        cout << "Case " << tc << ": " << l << endl;
     }
-    cin>>n;
-     for(int i=0;i<n;i++) {
-          cin>>val;
-           int it= lower_bound(v.begin(), v.end(), val)-v.begin();
-           cout<<it<< " " ;
-     }
-     cout<<endl; 
+
     return 0;
 }
