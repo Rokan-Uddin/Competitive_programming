772379805be90f240aefc9dfcba5718a1151720e
Rokan Uddin <rokanu55@gmail.com>
2021-01-17 19:42:06 +0000

    474B - Worms

--- main.cpp
+++ main.cpp
@@ -4,47 +4,25 @@ using namespace std;
 #define endl '\n';
 typedef long long ll;
 const ll infLL = 9000000000000000000;
-
 const int mx = 1e3+123;
 int a[mx], n;
-
-bool check ( ll val )
-{
-    for ( int i = 1; i <= n; i++ ) {
-        val += a[i];
-        if ( val <= 0 ) return 0;
-    }
-
-    return 1;
-}
-
 int main()
 {
     optimize();
-
-    int t;
-    cin >> t;
-
-    while ( t-- ) {
-
-        cin >> n;
-        for ( int i = 1; i <= n ; i++ ) cin >> a[i];
-
-        ll l = 0, r = 10e10+123;
-
-        ll ans = infLL;
-        while ( l <= r ) {
-            ll mid = ( l + r ) >> 1;
-
-            if ( check( mid ) ) {
-                ans = min ( ans, mid );
-                r = mid-1;
-            }
-            else l = mid+1;
-        }
-
-        cout << ans << endl;
+    int n,val,t;
+    cin>>n;
+     vector<int>v(n+1);
+    v[0]=0;
+    for(int i=1;i<=n;i++) {
+      cin>>val;
+      v[i]= val+v[i-1];
     }
-
+    cin>>n;
+     for(int i=0;i<n;i++) {
+          cin>>val;
+           int it= lower_bound(v.begin(), v.end(), val)-v.begin();
+           cout<<it<< " " ;
+     }
+     cout<<endl; 
     return 0;
 }
