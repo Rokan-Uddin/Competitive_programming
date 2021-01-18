ffa11cf9a570c81505d0be3d153a9a2cade07b71
Rokan Uddin <rokanu55@gmail.com>
2021-01-18 01:08:47 +0000

    Polycarp Training

--- main.cpp
+++ main.cpp
@@ -4,30 +4,17 @@ using namespace std;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
 
-
 int main()
 {
     optimize();
-    int t;
-    cin >> t;
-    while(t--) {
-      int n,val;
-      map<int,int>m;
-      m.clear();
-      cin>>n;
-      int  ta=0;
-      for(int i=0;i<n;i++) {
-        cin>>val;
-        m[val]++;
-        ta= max(m[val],ta);
-      }
-      int tb=m.size()-1;
-      int ans=0;
-      if(tb<(ta-1)) ans= tb+1;
-      else if(tb>ta) ans= ta;
-      else ans =tb;
-      cout<< ans <<endl;
-         
+    int n,ans=1;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++) cin>>v[i];
+    sort(v.begin(),v.end());
+    for(auto u:v) {
+      if(u>=ans) ans++;
     }
+    cout<<ans-1<<endl;
     return 0;
 }
