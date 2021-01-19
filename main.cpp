7e7bf7094b48e4f35dc0d3286529199a292087f2
Rokan Uddin <rokanu55@gmail.com>
2021-01-19 18:37:33 +0000

    Different Divisors

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
+//In the name of ALLAH
 #include<bits/stdc++.h>
 using namespace std;
 #define endl '\n'
+#define MOD 1000000007
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int isprime(long long  n) {
+    if(n<=1) return 0;
+    for(int i=2;i*i<=n;i++) {
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
 int main()
 {
     optimize();
     int t;
-    string s;
     cin>>t;
     while(t--) {
-      cin>>s;
-      int mx=0,l=0,r=0;
-      for(int i=0;i<s.size();i++) {
-        if(s[i]=='L') l++, r=0;
-        else if(s[i]=='R') r++,l=0;
-        mx=max(l,mx);
-      }
-      cout<<mx+1<<endl;
+    long long d,a=0,b=0;
+   cin>>d;
+   for(int i=1+d;;i++) {
+    if(isprime(i) && a==0) a=i, i=i+(d-1);
+    else if(isprime(i)) {   b=i; break; }
+   }
+   cout<<a*b<<endl;
     }
+
     return 0;
 }
