a61ae5a3ece0cc5d5ac83e00298170fab66aa505
Rokan Uddin <rokanu55@gmail.com>
2021-01-19 22:34:41 +0000

    Almost Prime

--- main.cpp
+++ main.cpp
@@ -3,22 +3,43 @@
 using namespace std;
 #define endl '\n'
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-const int mx = 1e7+123;
+const int mx = 1e6+123;
+bool is_prime[mx];
+vector<int> prime;
+void primeGen ( int n )
+{
+    for ( int i = 3; i <= n; i += 2 ) is_prime[i] = 1;
+ 
+    int sq = sqrt ( n );
+    for ( int i = 3; i <= sq; i += 2 ) {
+        if ( is_prime[i] == 1 ) {
+            for ( int j = i*i; j <= n; j += ( i + i ) ) {
+                is_prime[j] = 0;
+            }
+        }
+    }
+ 
+    is_prime[2] = 1;
+    prime.push_back (2);
+ 
+    for ( int i = 3; i <= n; i += 2 ) {
+        if ( is_prime[i] == 1 ) prime.push_back ( i );
+    }
+}
 int main()
 {
     optimize();
     int n;
-    cin >> n;
-    vector<int>divisor[n];
-    for ( int i = 1; i <= n; i++ ) {
-        for ( int j = i; j <= n; j += i ) {
-            divisor[j].push_back(i);
-        }
+    cin>>n;
+    primeGen(n);
+    map<int,int>mp;
+    for(int i=6;i<=n;i++) {
+      for(int j=0;j<prime.size();j++) if(i%prime[j]==0) mp[i]++;
     }
-    for(int i=1;i<=n;i++) {
-      cout<<i<<":->";
-      for(auto u:divisor[i]) cout<<u<<" ";
-      cout<<endl;
+        int ans=0;
+    for(auto u:mp) {
+      if(u.second==2) ans++;
     }
+    cout<<ans<<endl;
     
 }
