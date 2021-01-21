cd6d8d6ac0bf0df0d8337a08faee54687efc82c1
Rokan Uddin <rokanu55@gmail.com>
2021-01-21 21:11:26 +0000

    Multiply by 2, divide by 6

--- main.cpp
+++ main.cpp
@@ -6,18 +6,22 @@ using namespace std;
  
 int main() {
     long long t,n;
-    cin>> t;
-    while(t--){
-        cin>> n;
-        if((n/2)%2==1) cout<<"NO";
-        else {
-            cout<<"YES"<<endl;
-            for(long long i=2;i<=n;i=i+2) cout<<i<<" ";
-            for(long long i=1;i<n;i=i+2) {
-                    if(i==(n-1)) cout<<i+(n/2)<<" ";
-                    else cout<<i<<" ";
-        }
+    cin>>t;
+    while(t--) { 
+    cin>> n;
+    long long ans=0;
+    while(1) {
+      if(n==1) break; 
+      else if(n%6 ==0 ) {
+        n=n/6;
+        ans++;
+      }
+      else if(n%6==2 || n%6==4 || n%6==5) {ans =-1;break;}
+      else {
+        n=n*2; 
+        ans++;
+      }
     }
-    cout<<endl;
-  }
+    cout<<ans<<endl;
+    }
 }
