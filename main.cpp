bb2897811b9a5a160da5e130c784e5a2bb1a42ba
Rokan Uddin <rokanu55@gmail.com>
2021-01-21 20:31:20 +0000

    Balanced Array

--- main.cpp
+++ main.cpp
@@ -1,42 +1,23 @@
 //In the name of ALLAH
 #include<bits/stdc++.h>
 using namespace std;
-#define endl '\n'
+#define endl '\n';
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-const int mx = 1e6+123;
-bool is_prime[mx];
-vector<int> prime;
-void primeGen ( int n )
-{
-    for ( int i = 3; i <= n; i += 2 ) is_prime[i] = 1;
  
-    int sq = sqrt ( n );
-    for ( int i = 3; i <= sq; i += 2 ) {
-        if ( is_prime[i] == 1 ) {
-            for ( int j = i*i; j <= n; j += ( i + i ) ) {
-                is_prime[j] = 0;
-            }
+int main() {
+    long long t,n;
+    cin>> t;
+    while(t--){
+        cin>> n;
+        if((n/2)%2==1) cout<<"NO";
+        else {
+            cout<<"YES"<<endl;
+            for(long long i=2;i<=n;i=i+2) cout<<i<<" ";
+            for(long long i=1;i<n;i=i+2) {
+                    if(i==(n-1)) cout<<i+(n/2)<<" ";
+                    else cout<<i<<" ";
         }
     }
- 
-    is_prime[2] = 1;
-    prime.push_back (2);
- 
-    for ( int i = 3; i <= n; i += 2 ) {
-        if ( is_prime[i] == 1 ) prime.push_back ( i );
-    }
-}
-int main()
-{
-    optimize();
-    primeGen(mx);
-    int n,k;
-    cin>>n>>k;
-    int ans=0;
-    for(int i=1;prime[i-1]<=n;i++) {
-      if(is_prime[prime[i-1]+prime[i]+1]==1 && (prime[i-1]+prime[i]+1)<=n ) ans++;
-    }
-    if(ans>=k) cout<<"YES"<<endl;
-    else cout<<"NO"<<endl;
-    
+    cout<<endl;
+  }
 }
