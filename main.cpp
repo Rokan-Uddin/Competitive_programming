ddc015cbd983d944c75bcac51d4efef8deccf635
Rokan Uddin <rokanu55@gmail.com>
2021-01-23 22:52:23 +0000

    Decoding

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
 //In the name of ALLAH
 //MD Rokan Uddin
-//CSE_CU
 #include<bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-
 #define endl '\n'
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
@@ -13,14 +9,18 @@ typedef long long ll;
 int main()
 {
     optimize();
-    ll a,b;
-    cin>>a>>b;
-    if((b-a)%2==0) cout<<"NO"<<endl;
-    else {
-        cout<<"YES"<<endl;
-        for(ll i=a;i<=b;i=i+2) {
-        cout<<i<<" "<<i+1<<endl;
+    int n;
+    string s;
+    cin>>n;
+    cin>>s;
+    vector<char>v;
+    v.push_back(s[0]);
+    for(int i=1;i<n;i++) {
+        if(i%2 !=0) v.insert(v.begin(),s[i]);
+        else v.push_back(s[i]);
     }
-    }
+    if(n%2==0) reverse(v.begin(),v.end());
+    for(auto u:v) cout<<u;
+    cout<<endl;
     return 0;
 }
