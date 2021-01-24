356b501415cd7775e0dc4a736a54a25c178ef6bd
Rokan Uddin <rokanu55@gmail.com>
2021-01-24 04:32:10 +0000

    virtual contest 1

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
-//In the name of ALLAH
-//MD Rokan Uddin
+//Bismillahir rahmanir rahim
+
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
 #define endl '\n'
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
-
 int main()
 {
     optimize();
-    int n;
-    string s;
-    cin>>n;
-    cin>>s;
-    vector<char>v;
-    v.push_back(s[0]);
-    for(int i=1;i<n;i++) {
-        if(i%2 !=0) v.insert(v.begin(),s[i]);
-        else v.push_back(s[i]);
+    ll x,y,a,b;
+    cin>>x>>y>>a>>b;
+    vector<pair<ll,ll>>v;
+    for(ll i=b;i<=y;i++) {
+        for(ll j=a;j<=x;j++) {
+            if(j>i) v.push_back({j,i});
+        }
     }
-    if(n%2==0) reverse(v.begin(),v.end());
-    for(auto u:v) cout<<u;
-    cout<<endl;
+    sort(v.begin(),v.end());
+    cout<<v.size()<<endl;
+    for(auto u:v) cout<<u.first<<" "<<u.second<<endl;
+
     return 0;
 }
