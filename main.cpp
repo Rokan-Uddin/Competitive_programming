a8dfb3fe02e0d08a543f70fe4e40d6ccd4ae3976
Rokan Uddin <rokanu55@gmail.com>
2021-01-24 04:33:47 +0000

    virtual contest 2

--- main.cpp
+++ main.cpp
@@ -2,24 +2,34 @@
 
 #include<bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
 typedef vector<int> vi;
+typedef vector<ll> vl;
 #define endl '\n'
+const double PI = acos(-1);
+const double eps = 1e-9;
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+
+
+
 int main()
 {
     optimize();
-    ll x,y,a,b;
-    cin>>x>>y>>a>>b;
-    vector<pair<ll,ll>>v;
-    for(ll i=b;i<=y;i++) {
-        for(ll j=a;j<=x;j++) {
-            if(j>i) v.push_back({j,i});
-        }
+    ll n,a,b,mn=1e9,mx=0;
+    cin>>n;
+    vector<pair<pair<ll,ll>,ll>>v;
+    for(int i=1;i<=n;i++) {
+        cin>>a>>b;
+        v.push_back( { {a,(-1*b)},i } );
+        mn= min(mn,a);
+        mx=max(mx,b);
     }
     sort(v.begin(),v.end());
-    cout<<v.size()<<endl;
-    for(auto u:v) cout<<u.first<<" "<<u.second<<endl;
-
+    if(v[0].first.first==mn && v[0].first.second== (-1*mx) ) {
+        cout<<v[0].second<<endl;
+    }
+    else cout<<"-1"<<endl;
     return 0;
 }
