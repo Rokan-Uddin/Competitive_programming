5e99f2fffba545f7421825c9645c5880b6d1d356
Rokan Uddin <rokanu55@gmail.com>
2021-01-25 05:24:45 +0000

    Preparation for International Women's Day

--- main.cpp
+++ main.cpp
@@ -3,28 +3,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-typedef vector<int> vi;
 #define endl '\n'
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 int main()
 {
     optimize();
-    ll t,n,val;
-    cin>>t;
-    while(t--) {
-        cin>>n;
-        set<ll>s;
-        ll ans=0;
-      for(ll i=0;i<n;i++) { cin>>val; if(val%2==0) s.insert(val); }
-      while(s.size()!=0) {
-        auto it= s.crbegin();
-        auto k= *it;
-        s.erase(*it);
-        if(k%2==0) {
-            s.insert(k/2);
-            ans++;
-         }
-        }
-      cout<<ans<<endl;
+    ll n,k,val,ans=0;
+    vector<ll>v;
+    map<ll,ll>mp;
+    cin>>n>>k;
+    for(ll i=0;i<n;i++) {
+            cin>>val;
+            val= val%k;
+            v.push_back(val);
+            mp[val]++;
     }
+    sort(v.begin(),v.end());
+    for(ll i=0;i<v.size();i++) {
+        ll m= (k-v[i]);
+        if(m==k) m=0;
+        if(binary_search(v.begin()+i,v.end(),m)) {
+            mp[v[i]]--;
+            ll j= lower_bound(v.begin(),v.end(),m)-v.begin();
+            if(mp[v[j]]>0 && i!=j) {
+                if((v[i]+v[j])%k==0) {
+                    mp[v[j]]--;
+                    ans++;
+                }
+            }
+            else mp[v[i]]++;
+        }
+    }
+    cout<<ans*2<<endl;
 }
