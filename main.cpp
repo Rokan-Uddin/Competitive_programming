caaf3406f503e23a67017c772651c0c02b191bff
Rokan Uddin <rokanu55@gmail.com>
2021-01-28 18:14:30 +0000

    Contests Nezzar and Colorful Balls

--- main.cpp
+++ main.cpp
@@ -1,20 +1,27 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define optimize() ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
-#define ll long long
-#define endl "\n"
-int main(){
+typedef long long ll;
+#define endl '\n'
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+int main()
+{
     optimize();
-        string s;
-        cin >> s;
-        ll ans = 0;
-        while(s.size() != 1){
-            ans++;
-            ll sum = 0;
-            for(char c: s){
-                sum += c - '0';
-            }
-            s = to_string(sum);
+    ll t;
+    cin>>t;
+    while(t--) {
+        ll n,val,ans=0;
+        map<ll,ll>v;
+        cin>>n;
+        while(n--) {
+            cin>>val;
+            v[val]++;
+            ans= max(v[val],ans);
         }
-        cout << ans << endl;
+        cout<<ans<<endl;
+    }
+    return 0;
 }
