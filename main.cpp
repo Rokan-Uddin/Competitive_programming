258b2d9aa4b8143f7a1fe7fc87d2b41982f1512c
Rokan Uddin <rokanu55@gmail.com>
2021-01-28 18:50:04 +0000

    Mahmoud and a Triangle

--- main.cpp
+++ main.cpp
@@ -1,32 +1,34 @@
-//In the name of ALLAH
-//MD Rokan Uddin
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 #define endl '\n'
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-bool check(long long n)
-{
-    long long q = n;
-    while(q != 0)
-    {
-        int r = q%10;
-        if(r != 0 && n % r != 0)
-            return false;
-        q = q/10;
-    }
-    return true;
-}
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
 int main()
 {
     optimize();
-    ll t,n;
-    cin>>t;
-    while(t--) {
-        cin>>n;
-        while(check(n)==false) n++;
-        cout<<n<<endl;
+     int x,n=0;
+     cin>>x;
+     vector<int>a(x);
+    for(int i=0;i<x;i++) cin>>a[i];
+    sort(a.begin(),a.end());
+    for(int i=0;i<x-2;i++)
+    {
+        if(a[i]+a[i+1]>a[i+2])
+        {
+            cout<<"YES"<<endl;
+            n=1;
+            return 0;
+        }
     }
- 
+    if(n==0)
+    {
+       cout<<"NO"<<endl;
+    }
+
     return 0;
 }
+
+
