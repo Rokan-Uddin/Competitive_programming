fc67b470d2992b6c8f94927c6ae7b66a54d512f7
Rokan Uddin <rokanu55@gmail.com>
2021-01-28 18:15:53 +0000

    Fair Numbers

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
+//In the name of ALLAH
+//MD Rokan Uddin
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 #define endl '\n'
-#define mem(a,b) memset(a, b, sizeof(a) )
-#define sqr(a) ((a) * (a))
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
- 
+bool check(long long n)
+{
+    long long q = n;
+    while(q != 0)
+    {
+        int r = q%10;
+        if(r != 0 && n % r != 0)
+            return false;
+        q = q/10;
+    }
+    return true;
+}
 int main()
 {
     optimize();
-    ll t;
+    ll t,n;
     cin>>t;
     while(t--) {
-        ll n,val,ans=0;
-        map<ll,ll>v;
         cin>>n;
-        while(n--) {
-            cin>>val;
-            v[val]++;
-            ans= max(v[val],ans);
-        }
-        cout<<ans<<endl;
+        while(check(n)==false) n++;
+        cout<<n<<endl;
     }
+ 
     return 0;
 }
