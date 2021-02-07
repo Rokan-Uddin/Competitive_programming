bf6f791f82a96de98755f34c57021b77a06c9381
Rokan Uddin <rokanu55@gmail.com>
2021-02-07 21:08:09 +0000

    shooting

--- main.cpp
+++ main.cpp
@@ -2,33 +2,25 @@
 using namespace std;
 typedef long long ll;
 #define endl '\n'
-#define mem(a,b) memset(a, b, sizeof(a) )
-#define sqr(a) ((a) * (a))
+#define PB push_back
 #define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
 int main()
 {
-    optimize();
-     int x,n=0;
-     cin>>x;
-     vector<int>a(x);
-    for(int i=0;i<x;i++) cin>>a[i];
-    sort(a.begin(),a.end());
-    for(int i=0;i<x-2;i++)
-    {
-        if(a[i]+a[i+1]>a[i+2])
-        {
-            cout<<"YES"<<endl;
-            n=1;
-            return 0;
-        }
-    }
-    if(n==0)
-    {
-       cout<<"NO"<<endl;
-    }
-
+int n;
+	cin >> n;
+	vector<pair<int,int>> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i].first, v[i].second = i + 1;
+ 
+	sort(v.begin(),v.end(), greater<pair<int,int>>());
+	int s = 1;
+	for (int i = 1; i < n; i++) {
+		s = s + (v[i].first * i) + 1;
+	}
+	cout<<s<<endl;
+	for (int i = 0; i < n; i++)
+		cout << v[i].second << " ";
+	cout << endl;
     return 0;
 }
-
-
