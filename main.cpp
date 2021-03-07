e3c063763ff76c540f8c22aa6dd127bbfccd6e45
Rokan Uddin <rokanu55@gmail.com>
2021-03-07 21:41:22 +0000

    bfs

--- main.cpp
+++ main.cpp
@@ -1,43 +1,102 @@
+/** In the Name of ALLAH */
+ 
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define endl '\n'
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+int dx[] = {0, 0, +1, -1, +1, +1, -1, -1};
+int dy[] = {+1, -1, 0, 0, +1, -1, +1, -1};
+ 
+inline ll gcd ( ll a, ll b ) { return __gcd (a, b); }
+inline ll lcm ( ll a, ll b ) { return ( a * ( b / gcd (a, b) ) ); }
+ 
+vector<int>v[11];
+int l[11];
+int vis[11];
+int par[11];
+void bfs(int x) {
+  vis[x]=1;
+  l[x]=0;
+  queue<int>q;
+  q.push(x);
+  while(!q.empty()) {
+    int t=q.front();
+    q.pop();
+    for(int i=0;i<v[t].size();i++) {
+      if(vis[v[t][i]]== -1) {
+        vis[v[t][i]]=1;
+        q.push(v[t][i]);
+        l[v[t][i]]= l[t]+1;
+        par[v[t][i]]=t;
+      }
+    }
+  }
+}
+void path(int m) {
+  if(m==1) return ;
+  cout<<par[m]<<" ";
+  path(par[m]);
 
-const int MX = 1e7+123;
-bitset<MX> is_prime;
-vector<int> prime;
- 
-void primeGen ( int n )
-{
-    n += 100;
-    for ( int i = 3; i <= n; i += 2 ) is_prime[i] = 1;
- 
-    int sq = sqrt ( n );
-    for ( int i = 3; i <= sq; i += 2 ) {
-        if ( is_prime[i] == 1 ) {
-            for ( int j = i*i; j <= n; j += ( i + i ) ) {
-                is_prime[j] = 0;
-            }
-        }
-    }
- 
-    is_prime[2] = 1;
-    prime.push_back (2);
- 
-    for ( int i = 3; i <= n; i += 2 ) {
-        if ( is_prime[i] == 1 ) prime.push_back ( i );
-    }
 }
-int NOD (long long n) 
+int main()
 {
-    int ret = 1;
-    for ( auto p : prime ) { 
-        if ( 1LL * p * p > n ) break;
-        if ( n % p == 0 ) { 
-            int cnt = 1;
-            while ( n % p == 0 ) { 
-                n /= p; 
-                cnt++; 
-            }
-            ret *= cnt; 
-        }
-    }
-    if ( n > 1 ) ret *= 2; 
-    return ret;
+  optimize();
+  int n,k;
+  cin>>n>>k;
+  for(int i=0;i<k;i++) {
+    int x,y;
+    cin>>x>>y;
+    v[x].PB(y);
+    v[y].PB(x); 
+  }
+  mem(vis,-1);
+  bfs(1);
+  path(5);
 }
+// 1 2
+// 1 3
+// 1 4
+// 2 6
+// 4 7
+// 3 7
+// 3 8
+// 8 5
+// 5 10
+// 9 10
+// 6 10
+// 7 9
+// 7 8
